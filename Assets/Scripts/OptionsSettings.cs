24a4c56f0344757c291785db6d837e817be8e804
David Keane <zilonkeane@gmail.com>
2019-03-25 23:20:05 -0700

    No scripts are able to be placed on objects, but everything looks good

---
+++ Assets/Scripts/OptionsSettings.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsSettings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void SettingsSpeed( float setSpeed)
+    {
+ 
+    }
+    
+    public void SettingsBulletLimit( float setBulletLimit)
+    {
+
+    }
+
+    public void SettingsExplosiveMax( float setExplosiveMax)
+    {
+
+    }
+
+    public void SettingsWinLimit( float setWinLimit)
+    {
+
+    }
+}
