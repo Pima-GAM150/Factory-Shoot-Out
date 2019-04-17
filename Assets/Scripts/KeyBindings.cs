4413d316fe1692f7042653f7b55d8d3dca7bac6c
David Keane <zilonkeane@gmail.com>
2019-04-16 18:24:43 -0700

    Made Buttons that allow user to change controls

---
+++ Assets/Scripts/KeyBindings.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class KeyBindings : MonoBehaviour
+{
+    private Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>();
+
+    public TextMeshProUGUI shoot, reload, barrel, up, down, left, right;
+
+    private GameObject currentKey;
+
+    private Color normal = new Color32(39, 171, 249, 255);
+    private Color selected = new Color32(239, 116, 36, 255);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keys.Add("Shoot", KeyCode.Mouse0);
+        keys.Add("Reload", KeyCode.Mouse1);
+        keys.Add("Barrel", KeyCode.Space);
+        keys.Add("Up", KeyCode.W);
+        keys.Add("Down", KeyCode.S);
+        keys.Add("Left", KeyCode.A);
+        keys.Add("Right", KeyCode.D);
+
+        shoot.text = keys["Shoot"].ToString();
+        reload.text = keys["Reload"].ToString();
+        barrel.text = keys["Barrel"].ToString();
+        up.text = keys["Up"].ToString();
+        down.text = keys["Down"].ToString();
+        left.text = keys["Left"].ToString();
+        right.text = keys["Right"].ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    private void OnGUI()
+    {
+        if(currentKey != null)
+        {
+            Event e = Event.current;
+            if (e.isKey)
+            {
+                keys[currentKey.name] = e.keyCode;
+                currentKey.GetComponentInChildren<TextMeshProUGUI>().text = e.keyCode.ToString();
+                currentKey.GetComponent<Image>().color = normal;
+                currentKey = null;
+            }
+        }
+    }
+
+    public void ChangeKey(GameObject clicked)
+    {
+        if(currentKey != null)
+        {
+            currentKey.GetComponent<Image>().color = normal;
+        }
+        
+        currentKey = clicked;
+        currentKey.GetComponent<Image>().color = selected;
+
+    }
+}
