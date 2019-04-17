883afde88a5aa4f0cc21612f4d1a1d7b4730a22b
David Keane <zilonkeane@gmail.com>
2019-04-16 17:02:44 -0700

    Made barrels Instantiate and Destroy across the Network

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class Explosion : MonoBehaviour
 {
@@ -32,6 +33,6 @@ public class Explosion : MonoBehaviour
     }
     public void DoneExploding()
     {
-        Destroy(this.gameObject);
+        PhotonNetwork.Destroy(this.gameObject);
     }
 }
